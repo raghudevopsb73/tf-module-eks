8fef64da5beec79672f36eb1aa6ea6ea7f96ba5f
Raghu K <raghuk.vit@gmail.com>
2023-08-04 08:56:31 +0530

    Update tf-module-eks/

--- iam.tf
+++ iam.tf
@@ -55,3 +55,64 @@ resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
   role       = aws_iam_role.node-role.name
 }
+
+
+resource "aws_iam_policy" "sa-policy" {
+  name        = "eks-${var.env}-ssm-pm-policy"
+  path        = "/"
+  description = "eks-${var.env}-ssm-pm-policy"
+
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "VisualEditor0",
+        "Effect" : "Allow",
+        "Action" : [
+          "ssm:GetParameterHistory",
+          "ssm:GetParametersByPath",
+          "ssm:GetParameters",
+          "ssm:GetParameter",
+          "kms:Decrypt"
+        ],
+        "Resource" : [
+          "arn:aws:ssm:us-east-1:739561048503:parameter/roboshop.*",
+          var.kms_arn
+        ]
+      }
+    ]
+  })
+}
+
+## Iam Role
+
+resource "aws_iam_role" "sa-role" {
+  name = "eks-${var.env}-ssm-pm-role"
+
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : "sts:AssumeRoleWithWebIdentity",
+        "Principal" : {
+          "Federated" : aws_iam_openid_connect_provider.cluster.arn
+        },
+        "Condition" : {
+          "StringEquals" : {
+            "oidc.eks.us-east-1.amazonaws.com/id/${element(split("/", aws_iam_openid_connect_provider.cluster.arn), 3)}:aud" : [
+              "sts.amazonaws.com"
+            ]
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "policy-attach" {
+  role       = aws_iam_role.sa-role.name
+  policy_arn = aws_iam_policy.sa-policy.arn
+}
+
+
--- variables.tf
+++ variables.tf
@@ -4,3 +4,4 @@ variable "min_size" {}
 variable "max_size" {}
 variable "capacity_type" {}
 variable "instance_types" {}
+variable "kms_arn" {}
