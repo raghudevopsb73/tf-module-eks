1ab2d8cab3f7a149f28df9488c97b1fca4392691
Raghu K <raghuk.vit@gmail.com>
2023-08-11 08:40:08 +0530

    Update tf-module-eks/

--- nginx-ingress.tf
+++ nginx-ingress.tf
@@ -13,6 +13,7 @@ aws eks update-kubeconfig --name ${var.env}-eks
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo update
 helm upgrade -i ingress ingress-nginx/ingress-nginx -f ${path.module}/nginx-values.yml
+kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
 EOF
   }
 }
